009ef0f6512c963102a07c1da72068873cf9f089
aesclone <43333330+aesclone@users.noreply.github.com>
2018-09-28 03:01:27 -0500

    Added an example for PlaneList

--- Main.py
+++ Main.py
@@ -6,8 +6,19 @@
 import PistonEngine
 import TurboJet
 import ElectricMotor
+import Plane
 
 a = PistonEngine.PistonEngine("NACA_2412_Re5.256.txt", "Cessna 172", 11, 1.378, 0, 1.11, 2, 62, 84, 767, 173, 171,
                               0.0175, 0.7, 0.000000777, .9, 120000, 6)
-a.plot_data()
+
+b = PistonEngine.PistonEngine("NACA_2412_Re5.256.txt", "Cessna 172-1", 11, 1.378, 0, 1.11, 2, 62, 84, 747, 173, 190,
+                              0.0175, 0.7, 0.000000777, .9, 120000, 6)
+
+lst = Plane.PlaneList()
+lst.add_plane(a)
+lst.add_plane(b)
+
+c = lst.greatest_range()
+
+print(c.get_name())
 
