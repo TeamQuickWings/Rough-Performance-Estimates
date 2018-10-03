daeea8ed3a75d1fbace50c146a4ba0d557c5eb19
kushaldevsuresh <43591538+kushaldevsuresh@users.noreply.github.com>
2018-10-03 08:28:34 +0530

    Added method

---
+++ AirDensity.py
@@ -0,0 +1,36 @@
+import math
+
+aT=-(6.5*(10**-3))
+TSL=288.16
+d=1.225
+g0=9.80065
+R=287
+
+
+def get_air_density(h):
+
+    if h == 0:
+
+        return d
+
+    elif 0 < h <= 11000:
+
+        temp = TSL + aT * h
+        rho = (temp / TSL)**((-(g0 / (aT * R)))-1) * d
+        return rho
+
+    elif 11000 < h <= 25000:
+
+        temp = TSL + aT * 11000
+        rho0 = (temp / TSL) ** ((-(g0 / (aT * R)))-1) * d
+        rho = math.exp(-(g0 / (R * temp))*(h - 11000)) * rho0
+        return rho
+
+    elif h > 25000:
+
+        aS = 3*(10 ** -3)
+        temp = (TSL + aT * 11000) + aS*(h-25000)
+        temp0 = TSL + aT * 11000
+        rho0 = math.exp(-(g0 / (R * temp0)) * (h - 11000)*(temp0 / TSL)**(-(g0/aT)*R)-1)*d
+        rho = (temp / temp0) ** ((-(g0 / (aS * R)))-1) * rho0
+        return rho
