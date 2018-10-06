d1dbb623534ccf22ad9df2588bab1f2151fbadca
aesclone <43333330+aesclone@users.noreply.github.com>
2018-10-05 21:50:48 -0500

    Working on adding cost estimations

---
+++ CostEstimate.py
@@ -0,0 +1,13 @@
+# Nathan Briggs, Andrew Scherping
+# Code to estimate the operating costs of an aircraft
+
+num_engines = 1
+take_off_mass = 900
+block_distance = 500
+cruise_velocity = 180
+
+years_of_operation = 20
+crew_hours = 800
+time_to_cruise = 0.42
+
+
